7203fe28c63df65da2b514ae08549d07c040b12b
Duknam Yoo <yoo2001818@gmail.com>
2019-03-21 23:52:16 +0900

    Add basic types

---
+++ src/type.ts
@@ -0,0 +1,22 @@
+export type CompareExpression<T> = {
+  type: '<' | '>',
+  value: T,
+  equal: boolean,
+  excludes?: T[],
+};
+export type RangeExpression<T> = {
+  type: 'range',
+  min: T,
+  max: T,
+  minEqual: boolean,
+  maxEqual: boolean,
+  excludes?: T[],
+};
+export type EqExpression<T> = { type: '=', value: T };
+
+export type Expression<T> = CompareExpression<T> | RangeExpression<T> |
+  EqExpression<T>;
+
+export type WildcardRangeSet<T> = { type: '*', excludes?: T[] };
+
+export type RangeSet<T> = Expression<T>[] | WildcardRangeSet<T> | true | false;
